4261f02788dd8bb063e2da8735b192c87c79570f
Vivek Rawat <vivke.rawat888@gmail.com>
2021-01-09 11:41:17 +0530

    splite stats function into a different file

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 	"github.com/PuerkitoBio/goquery"
 	ui "github.com/gizak/termui/v3"
 	"github.com/gizak/termui/v3/widgets"
-	"golang.org/x/net/html"
 )
 
 const htSitemap = "https://www.hindustantimes.com/sitemap/news.xml"
@@ -251,48 +250,4 @@ func scrapeData(url string, i int, ch chan artText){
 	})
 	// send data back to the channel
 	ch <- *at
-}
-
-// count the no of HTML children
-func numberOfChild(n *html.Node) int {
-	if n == nil {
-		return -1
-	}
-	count := 0
-
-	// c is the current child
-	// loop until c is null and iterate the next sibling
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		count++
-	}
-
-	return count
-}
-
-// map the character in the given text
-func mapChar(s *string) map[string]int {
-	m := make(map[string]int)
-	for i := range *s {
-		m[string((*s)[i])]++
-	}
-	return m
-}
-
-// display stats
-func stats(s *string) {
-	// get the no. of characters
-	c := len(*s)
-	m := mapChar(s)
-
-	// divide the string to words
-	words := strings.Fields(*s)
-
-	fmt.Printf("no. of \ncharacters: %d\nwords: %d\nmap: %v", c, len(words), m)
-
-	// display in a different format
-	// sort the data
-	// print the map of the character
-	for i := range m {
-		fmt.Println(i, ":", m[i])
-	}
 }
---
+++ stats.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"strings"
+	"fmt"
+
+	"golang.org/x/net/html"
+)
+
+// count the no of HTML children
+func numberOfChild(n *html.Node) int {
+	if n == nil {
+		return -1
+	}
+	count := 0
+
+	// c is the current child
+	// loop until c is null and iterate the next sibling
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		count++
+	}
+
+	return count
+}
+
+// map the character in the given text
+func mapChar(s *string) map[string]int {
+	m := make(map[string]int)
+	for i := range *s {
+		m[string((*s)[i])]++
+	}
+	return m
+}
+
+// display stats
+func stats(s *string) {
+	// get the no. of characters
+	c := len(*s)
+	m := mapChar(s)
+
+	// divide the string to words
+	words := strings.Fields(*s)
+
+	fmt.Printf("no. of \ncharacters: %d\nwords: %d\nmap: %v", c, len(words), m)
+
+	// display in a different format
+	// sort the data
+	// print the map of the character
+	for i := range m {
+		fmt.Println(i, ":", m[i])
+	}
+}
