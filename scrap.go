c796094e668c1aa2a5cb87780ef7d8b9ae374014
Vivek Rawat <vivke.rawat888@gmail.com>
2021-01-13 08:54:51 +0530

    added second fetch function

--- scrap.go
+++ scrap.go
@@ -7,6 +7,9 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+// function identifier for scraping data
+type scraper func(*goquery.Document)
+
 // return goquery Document from a URL
 func fetchDoc(url string) (doc *goquery.Document) {
 	// get the response from the URL
@@ -29,4 +32,28 @@ func fetchDoc(url string) (doc *goquery.Document) {
 	errHandle("scrap: new document from reader", err)
 
 	return
+}
+
+func fetchDoc2(url string, scrap scraper) {
+	// get the response from the URL
+	res, err := http.Get(url)
+	errHandle("scrap: http get error", err)
+
+	// handle unsuccessful status code
+	if res.StatusCode != 200 {
+		errHandle("scrap: error code from the server", fmt.Errorf(
+			"%v:%v",
+			res.StatusCode,
+			htSitemap,
+		))
+	}
+
+	defer res.Body.Close()
+
+	// create a new document from the response
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	errHandle("scrap: new document from reader", err)
+
+	// run the scraper function
+	scrap(doc)
 }
